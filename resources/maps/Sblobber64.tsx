2a101e16170629ae540e0f86efb4de83a008f33a
Victor Vieux <victorvieux@gmail.com>
2021-02-27 18:23:46 -0800

    MAPS: tmx for tutorial

--- resources/maps/Sblobber64.tsx
+++ resources/maps/Sblobber64.tsx
@@ -1,250 +1,256 @@
-<?xml version = "1.0" encoding = "UTF-8" ?>
-    <tileset version="1.4" tiledversion="1.4.3" name="Sblobber64" tilewidth="32" tileheight="32" tilecount="41" columns="0">
-        <grid orientation="orthogonal" width="1" height="1" />
-        <tile id="0">
-            <properties>
-                <property name="char" value="" />
-            </properties>
-            <image width="32" height="32" source="../gfx/sprites/map/tile_h_empty.png" />
-        </tile>
-        <tile id="1">
-            <properties>
-                <property name="char" value="E" />
-            </properties>
-            <image width="32" height="32" source="../gfx/sprites/map/tile_d_top.png" />
-        </tile>
-        <tile id="2">
-            <properties>
-                <property name="char" value="d" />
-            </properties>
-            <image width="32" height="32" source="../gfx/sprites/map/tile_d_open.png" />
-        </tile>
-        <tile id="3">
-            <properties>
-                <property name="char" value="h" />
-            </properties>
-            <image width="32" height="32" source="../gfx/sprites/map/tile_h.png" />
-        </tile>
-        <tile id="4">
-            <properties>
-                <property name="char" value="g" />
-            </properties>
-            <image width="32" height="32" source="../gfx/sprites/map/tile_g.png" />
-        </tile>
-        <tile id="5">
-            <properties>
-                <property name="char" value="#" />
-            </properties>
-            <image width="32" height="32" source="../gfx/sprites/map/tile_end.png" />
-        </tile>
-        <tile id="6">
-            <properties>
-                <property name="char" value="@" />
-            </properties>
-            <image width="32" height="32" source="../gfx/sprites/map/tile_arrow_start.png" />
-        </tile>
-        <tile id="7">
-            <properties>
-                <property name="char" value="$" />
-            </properties>
-            <image width="32" height="32" source="../gfx/sprites/map/tile_arrow_end.png" />
-        </tile>
-        <tile id="8">
-            <properties>
-                <property name="char" value="=" />
-            </properties>
-            <image width="32" height="32" source="../gfx/sprites/map/tile_arrow.png" />
-        </tile>
-        <tile id="9">
-            <properties>
-                <property name="char" value="i" />
-            </properties>
-            <image width="32" height="32" source="../gfx/sprites/map/tile_w_ver_t.png" />
-        </tile>
-        <tile id="10">
-            <properties>
-                <property name="char" value="," />
-            </properties>
-            <image width="32" height="32" source="../gfx/sprites/map/tile_w_ver_b.png" />
-        </tile>
-        <tile id="11">
-            <properties>
-                <property name="char" value=")" />
-            </properties>
-            <image width="32" height="32" source="../gfx/sprites/map/tile_w_hor_r.png" />
-        </tile>
-        <tile id="12">
-            <properties>
-                <property name="char" value="(" />
-            </properties>
-            <image width="32" height="32" source="../gfx/sprites/map/tile_w_hor_l.png" />
-        </tile>
-        <tile id="13">
-            <properties>
-                <property name="char" value="w" />
-            </properties>
-            <image width="32" height="32" source="../gfx/sprites/map/tile_w.png" />
-        </tile>
-        <tile id="14">
-            <properties>
-                <property name="char" value="%" />
-            </properties>
-            <image width="32" height="32" source="../gfx/sprites/map/tile_tree_2.png" />
-        </tile>
-        <tile id="15">
-            <properties>
-                <property name="char" value="*" />
-            </properties>
-            <image width="32" height="32" source="../gfx/sprites/map/tile_tree_1.png" />
-        </tile>
-        <tile id="16">
-            <properties>
-                <property name="char" value="&amp;" />
-            </properties>
-            <image width="32" height="32" source="../gfx/sprites/map/tile_tree_0.png" />
-        </tile>
-        <tile id="17">
-            <properties>
-                <property name="char" value="p" />
-            </properties>
-            <image width="32" height="32" source="../gfx/sprites/map/tile_p.png" />
-        </tile>
-        <tile id="18">
-            <properties>
-                <property name="char" value="k" />
-            </properties>
-            <image width="32" height="32" source="../gfx/sprites/map/tile_k.png" />
-        </tile>
-        <tile id="19">
-            <properties>
-                <property name="char" value="." />
-            </properties>
-            <image width="32" height="32" source="../gfx/sprites/map/tile_grass.png" />
-        </tile>
-        <tile id="20">
-            <properties>
-                <property name="char" value="f" />
-            </properties>
-            <image width="32" height="32" source="../gfx/sprites/map/tile_f.png" />
-        </tile>
-        <tile id="21">
-            <properties>
-                <property name="char" value="L" />
-            </properties>
-            <image width="32" height="32" source="../gfx/sprites/map/tile_d_lock.png" />
-        </tile>
-        <tile id="22">
-            <properties>
-                <property name="char" value="D" />
-            </properties>
-            <image width="32" height="32" source="../gfx/sprites/map/tile_d_close.png" />
-        </tile>
-        <tile id="23">
-            <properties>
-                <property name="char" value="C" />
-            </properties>
-            <image width="32" height="32" source="../gfx/sprites/map/tile_c_up.png" />
-        </tile>
-        <tile id="24">
-            <properties>
-                <property name="char" value="c" />
-            </properties>
-            <image width="32" height="32" source="../gfx/sprites/map/tile_c_down.png" />
-        </tile>
-        <tile id="25">
-            <properties>
-                <property name="char" value="B" />
-            </properties>
-            <image width="32" height="32" source="../gfx/sprites/map/tile_b_up.png" />
-        </tile>
-        <tile id="26">
-            <properties>
-                <property name="char" value="b" />
-            </properties>
-            <image width="32" height="32" source="../gfx/sprites/map/tile_b_down.png" />
-        </tile>
-        <tile id="27">
-            <properties>
-                <property name="char" value="A" />
-            </properties>
-            <image width="32" height="32" source="../gfx/sprites/map/tile_a_up.png" />
-        </tile>
-        <tile id="28">
-            <properties>
-                <property name="char" value="a" />
-            </properties>
-            <image width="32" height="32" source="../gfx/sprites/map/tile_a_down.png" />
-        </tile>
-        <tile id="29">
-            <properties>
-                <property name="char" value="s" />
-            </properties>
-            <image width="32" height="32" source="../gfx/sprites/map/tile_s.png" />
-        </tile>
-        <tile id="30">
-            <properties>
-                <property name="char" value="e" />
-            </properties>
-            <image width="32" height="32" source="../gfx/sprites/map/tile_e.png" />
-        </tile>
-        <tile id="31">
-            <properties>
-                <property name="char" value="|" />
-            </properties>
-            <image width="32" height="32" source="../gfx/sprites/map/tile_w_ver.png" />
-        </tile>
-        <tile id="32">
-            <properties>
-                <property name="char" value="}" />
-            </properties>
-            <image width="32" height="32" source="../gfx/sprites/map/tile_w_tr.png" />
-        </tile>
-        <tile id="33">
-            <properties>
-                <property name="char" value="y" />
-            </properties>
-            <image width="32" height="32" source="../gfx/sprites/map/tile_w_tm.png" />
-        </tile>
-        <tile id="34">
-            <properties>
-                <property name="char" value="{" />
-            </properties>
-            <image width="32" height="32" source="../gfx/sprites/map/tile_w_tl.png" />
-        </tile>
-        <tile id="35">
-            <properties>
-                <property name="char" value="&lt;" />
-            </properties>
-            <image width="32" height="32" source="../gfx/sprites/map/tile_w_mr.png" />
-        </tile>
-        <tile id="36">
-            <properties>
-                <property name="char" value="&gt;" />
-            </properties>
-            <image width="32" height="32" source="../gfx/sprites/map/tile_w_ml.png" />
-        </tile>
-        <tile id="37">
-            <properties>
-                <property name="char" value="-" />
-            </properties>
-            <image width="32" height="32" source="../gfx/sprites/map/tile_w_hor.png" />
-        </tile>
-        <tile id="38">
-            <properties>
-                <property name="char" value="]" />
-            </properties>
-            <image width="32" height="32" source="../gfx/sprites/map/tile_w_br.png" />
-        </tile>
-        <tile id="39">
-            <properties>
-                <property name="char" value="^" />
-            </properties>
-            <image width="32" height="32" source="../gfx/sprites/map/tile_w_bm.png" />
-        </tile>
-        <tile id="40">
-            <properties>
-                <property name="char" value="[" />
-            </properties>
-            <image width="32" height="32" source="../gfx/sprites/map/tile_w_bl.png" />
-        </tile>
-    </tileset>
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="Sblobber64" tilewidth="32" tileheight="32" tilecount="42" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <properties>
+   <property name="char" value=""/>
+  </properties>
+  <image width="32" height="32" source="../gfx/sprites/map/tile_h_empty.png"/>
+ </tile>
+ <tile id="1">
+  <properties>
+   <property name="char" value="E"/>
+  </properties>
+  <image width="32" height="32" source="../gfx/sprites/map/tile_d_top.png"/>
+ </tile>
+ <tile id="2">
+  <properties>
+   <property name="char" value="d"/>
+  </properties>
+  <image width="32" height="32" source="../gfx/sprites/map/tile_d_open.png"/>
+ </tile>
+ <tile id="3">
+  <properties>
+   <property name="char" value="h"/>
+  </properties>
+  <image width="32" height="32" source="../gfx/sprites/map/tile_h.png"/>
+ </tile>
+ <tile id="4">
+  <properties>
+   <property name="char" value="g"/>
+  </properties>
+  <image width="32" height="32" source="../gfx/sprites/map/tile_g.png"/>
+ </tile>
+ <tile id="5">
+  <properties>
+   <property name="char" value="#"/>
+  </properties>
+  <image width="32" height="32" source="../gfx/sprites/map/tile_end.png"/>
+ </tile>
+ <tile id="6">
+  <properties>
+   <property name="char" value="@"/>
+  </properties>
+  <image width="32" height="32" source="../gfx/sprites/map/tile_arrow_start.png"/>
+ </tile>
+ <tile id="7">
+  <properties>
+   <property name="char" value="$"/>
+  </properties>
+  <image width="32" height="32" source="../gfx/sprites/map/tile_arrow_end.png"/>
+ </tile>
+ <tile id="8">
+  <properties>
+   <property name="char" value="="/>
+  </properties>
+  <image width="32" height="32" source="../gfx/sprites/map/tile_arrow.png"/>
+ </tile>
+ <tile id="9">
+  <properties>
+   <property name="char" value="i"/>
+  </properties>
+  <image width="32" height="32" source="../gfx/sprites/map/tile_w_ver_t.png"/>
+ </tile>
+ <tile id="10">
+  <properties>
+   <property name="char" value=","/>
+  </properties>
+  <image width="32" height="32" source="../gfx/sprites/map/tile_w_ver_b.png"/>
+ </tile>
+ <tile id="11">
+  <properties>
+   <property name="char" value=")"/>
+  </properties>
+  <image width="32" height="32" source="../gfx/sprites/map/tile_w_hor_r.png"/>
+ </tile>
+ <tile id="12">
+  <properties>
+   <property name="char" value="("/>
+  </properties>
+  <image width="32" height="32" source="../gfx/sprites/map/tile_w_hor_l.png"/>
+ </tile>
+ <tile id="13">
+  <properties>
+   <property name="char" value="w"/>
+  </properties>
+  <image width="32" height="32" source="../gfx/sprites/map/tile_w.png"/>
+ </tile>
+ <tile id="14">
+  <properties>
+   <property name="char" value="%"/>
+  </properties>
+  <image width="32" height="32" source="../gfx/sprites/map/tile_tree_2.png"/>
+ </tile>
+ <tile id="15">
+  <properties>
+   <property name="char" value="*"/>
+  </properties>
+  <image width="32" height="32" source="../gfx/sprites/map/tile_tree_1.png"/>
+ </tile>
+ <tile id="16">
+  <properties>
+   <property name="char" value="&amp;"/>
+  </properties>
+  <image width="32" height="32" source="../gfx/sprites/map/tile_tree_0.png"/>
+ </tile>
+ <tile id="17">
+  <properties>
+   <property name="char" value="p"/>
+  </properties>
+  <image width="32" height="32" source="../gfx/sprites/map/tile_p.png"/>
+ </tile>
+ <tile id="18">
+  <properties>
+   <property name="char" value="k"/>
+  </properties>
+  <image width="32" height="32" source="../gfx/sprites/map/tile_k.png"/>
+ </tile>
+ <tile id="19">
+  <properties>
+   <property name="char" value="."/>
+  </properties>
+  <image width="32" height="32" source="../gfx/sprites/map/tile_grass.png"/>
+ </tile>
+ <tile id="20">
+  <properties>
+   <property name="char" value="f"/>
+  </properties>
+  <image width="32" height="32" source="../gfx/sprites/map/tile_f.png"/>
+ </tile>
+ <tile id="21">
+  <properties>
+   <property name="char" value="L"/>
+  </properties>
+  <image width="32" height="32" source="../gfx/sprites/map/tile_d_lock.png"/>
+ </tile>
+ <tile id="22">
+  <properties>
+   <property name="char" value="D"/>
+  </properties>
+  <image width="32" height="32" source="../gfx/sprites/map/tile_d_close.png"/>
+ </tile>
+ <tile id="23">
+  <properties>
+   <property name="char" value="C"/>
+  </properties>
+  <image width="32" height="32" source="../gfx/sprites/map/tile_c_up.png"/>
+ </tile>
+ <tile id="24">
+  <properties>
+   <property name="char" value="c"/>
+  </properties>
+  <image width="32" height="32" source="../gfx/sprites/map/tile_c_down.png"/>
+ </tile>
+ <tile id="25">
+  <properties>
+   <property name="char" value="B"/>
+  </properties>
+  <image width="32" height="32" source="../gfx/sprites/map/tile_b_up.png"/>
+ </tile>
+ <tile id="26">
+  <properties>
+   <property name="char" value="b"/>
+  </properties>
+  <image width="32" height="32" source="../gfx/sprites/map/tile_b_down.png"/>
+ </tile>
+ <tile id="27">
+  <properties>
+   <property name="char" value="A"/>
+  </properties>
+  <image width="32" height="32" source="../gfx/sprites/map/tile_a_up.png"/>
+ </tile>
+ <tile id="28">
+  <properties>
+   <property name="char" value="a"/>
+  </properties>
+  <image width="32" height="32" source="../gfx/sprites/map/tile_a_down.png"/>
+ </tile>
+ <tile id="29">
+  <properties>
+   <property name="char" value="s"/>
+  </properties>
+  <image width="32" height="32" source="../gfx/sprites/map/tile_s.png"/>
+ </tile>
+ <tile id="30">
+  <properties>
+   <property name="char" value="e"/>
+  </properties>
+  <image width="32" height="32" source="../gfx/sprites/map/tile_e.png"/>
+ </tile>
+ <tile id="31">
+  <properties>
+   <property name="char" value="|"/>
+  </properties>
+  <image width="32" height="32" source="../gfx/sprites/map/tile_w_ver.png"/>
+ </tile>
+ <tile id="32">
+  <properties>
+   <property name="char" value="}"/>
+  </properties>
+  <image width="32" height="32" source="../gfx/sprites/map/tile_w_tr.png"/>
+ </tile>
+ <tile id="33">
+  <properties>
+   <property name="char" value="y"/>
+  </properties>
+  <image width="32" height="32" source="../gfx/sprites/map/tile_w_tm.png"/>
+ </tile>
+ <tile id="34">
+  <properties>
+   <property name="char" value="{"/>
+  </properties>
+  <image width="32" height="32" source="../gfx/sprites/map/tile_w_tl.png"/>
+ </tile>
+ <tile id="35">
+  <properties>
+   <property name="char" value="&lt;"/>
+  </properties>
+  <image width="32" height="32" source="../gfx/sprites/map/tile_w_mr.png"/>
+ </tile>
+ <tile id="36">
+  <properties>
+   <property name="char" value="&gt;"/>
+  </properties>
+  <image width="32" height="32" source="../gfx/sprites/map/tile_w_ml.png"/>
+ </tile>
+ <tile id="37">
+  <properties>
+   <property name="char" value="-"/>
+  </properties>
+  <image width="32" height="32" source="../gfx/sprites/map/tile_w_hor.png"/>
+ </tile>
+ <tile id="38">
+  <properties>
+   <property name="char" value="]"/>
+  </properties>
+  <image width="32" height="32" source="../gfx/sprites/map/tile_w_br.png"/>
+ </tile>
+ <tile id="39">
+  <properties>
+   <property name="char" value="^"/>
+  </properties>
+  <image width="32" height="32" source="../gfx/sprites/map/tile_w_bm.png"/>
+ </tile>
+ <tile id="40">
+  <properties>
+   <property name="char" value="["/>
+  </properties>
+  <image width="32" height="32" source="../gfx/sprites/map/tile_w_bl.png"/>
+ </tile>
+ <tile id="41">
+  <properties>
+   <property name="char" value="v"/>
+  </properties>
+  <image width="32" height="32" source="../gfx/sprites/map/tile_w.png"/>
+ </tile>
+</tileset>
